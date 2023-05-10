da96df59f6c4c5ec74812e2148c05536c0bcbf90
Kayla Toliver <kayla015.kt@gmail.com>
2023-05-09 22:32:17 -0500

    added assets folder and logo image

---
+++ main.js
@@ -0,0 +1,15 @@
+const store = document.querySelector('#store');
+
+const food = document.querySelector('#food');
+
+const toys = document.querySelector('#toys');
+
+const eat = document.querySelector('#eat');
+
+const play = document.querySelector('#play');
+
+const bath = document.querySelector('#bath');
+
+store.addEventListener('click', ()=>{
+alert('store test');
+});
